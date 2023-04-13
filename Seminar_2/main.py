32028c36201a830d0c3119818aa32e2accfc3171
Konstantin_Chern <codahl@yandex.ru>
2023-04-14 01:20:40 +0300

    Solved Task_11

--- Seminar_2/main.py
+++ Seminar_2/main.py
@@ -1,23 +1,19 @@
-# ; По данному целому неотрицательному n вычислите
-# ; значение n!. N! = 1 * 2 * 3 * … * N (произведение всех
-# ; чисел от 1 до N) 0! = 1 Решить задачу используя цикл
-# ; while
+number = int(input("введите число >1 ==>"))
 
-number = int(input("введите предел: "))
+first = 0
+second = 1
 
-fact = 1
-# i = 1
+index = 1
 
-# while i <= number:
-#     fact *= i
-#     i += 1
+while second < number:
+    first, second = second, first + second
+    index += 1
 
-for i in range(1, number+1):
-    fact *= i
-
-
-print(fact)
-
+print(index if second == number else -1 )
 
 
     
+
+
+
+ 
