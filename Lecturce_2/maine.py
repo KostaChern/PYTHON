7e00ee2f0832b80fa08b488268f032dab895ac91
Konstantin_Chern <codahl@yandex.ru>
2023-04-16 18:07:13 +0300

    Task_16 solved

---
+++ Lecturce_2/maine.py
@@ -0,0 +1,82 @@
+# list_1 = [1, 2, 3, 4, 5]
+# print(list_1)    # если в начале скобок поставить * то, при выводе на печать, будут убраны скобки и знаки препинания 
+# for i in list_1:
+#     print(i)
+# print()
+# print(len(list_1))
+
+# print()
+# print((list_1[3]))
+
+# list_1 = [1, 2]
+# print(list_1)
+# list_1.append(3)
+# print(list_1)
+
+# list_1 = []
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+# print(list_1)
+
+
+# list_1 = [12, 3, 43, 14, 4, 5]     #удаление элемента
+# # print(list_1)
+# # print(list_1.pop())
+# # print(list_1)
+# # a = list_1.pop()
+# # print(a)
+# print(list_1.pop(2))
+# print(list_1)
+
+
+# list_1 = [12, 3, 43, 14, 4, 5]       # добавление элемента на нужную позицию
+# print(list_1)
+# print(list_1.insert(3, 15))
+# print(list_1)
+
+
+# list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# # print(list_1[0])
+# # print(list_1[3])
+# # print(list_1[-1])
+# # print(list_1[-3])
+# # print(list_1[:])
+# # print(list_1[:3])                # вывод элементов с 3-го, но не включая 3-й
+# # print(list_1[len(list_1)-2:])
+# # print(list_1[2:8])               # вывод элементов со 2-го, но не включая 8-й
+# print(list_1[0:len(list_1):3])     # вывод элементов с начала до конца и с шагом 3
+# print(list_1[::3])                 # вывод элементов с начала до конца и с шагом 3
+
+
+# t = ()                 # КОРТЕЖ
+# print(type(t))
+
+# v = [1, 2, 3, 4]
+# print(v)
+# print(type(v))
+# v = tuple(v)
+# print(v)
+# print(type(v))
+# a, b, c, d = v
+# print(a, b, c, d)
+
+# v = (1, 2, 8, 4)
+# # print(v[2])
+# for i in v:
+#     print(i)
+# for i in range(len(v)):
+#     print(v[i])
+
+
+#              СЛОВАРИ
+
+# d = {}
+# d = dict()
+
+# d["q"] = "qwerty" 
+# print(d)
+# d["w"] = "werty" 
+# print(d)
+# print(d["q"])
+
